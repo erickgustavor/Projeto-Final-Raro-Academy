422c17b11e9e71c60b248ff1ebb9ff51c960884f
m3l4o <joaovictor.slv@ufpi.edu.br>
2024-10-24 11:51:44 -0300

    refactor: mudanca nas views para usar a classe do django

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import registration_form, process_registration, confirm_registration, login_view, login_process, home_view
+
+from .views import ConfirmView, LoginView, RegisterView, HomeView, LogoutView
 
 urlpatterns = [
-    path('register/', registration_form, name='registration_form'),
-    path('register/submit/', process_registration, name='process_registration'),
-    path('confirm/<int:account_id>/', confirm_registration, name='confirm_registration'),
-    path('login/', login_view, name='login_form'),
-    path('login/process/', login_process, name='login_process'),
-    path('home/', home_view, name='home')
+    path("register/", RegisterView.as_view(), name="register"),
+    path("confirm/<int:account_id>/", ConfirmView.as_view(), name="confirm"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("home/", HomeView.as_view(), name="home"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404, redirect, render
+from django.views import View
 from django.views.decorators.http import require_GET, require_POST
 
 from account.forms.login_forms import LoginForm
@@ -11,84 +12,89 @@ from account.forms.registration_forms import AccountRegistrationForm
 from .models import Account
 
 
-@require_GET
-def login_view(request):
-    form = LoginForm()
-    return render(request, "login/login_form.html", {"form": form})
+class RegisterView(View):
+    def get(self, request, *args, **kwargs):
+        form = AccountRegistrationForm()
+        return render(request, "registration/register.html", {"form": form})
+
+    def post(self, request, *args, **kwargs):
+        form = AccountRegistrationForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            username = form.cleaned_data.get("username")
+            cpf = form.cleaned_data.get("cpf")
+            password = form.cleaned_data.get("password")
+
+            account = Account.objects.create_user(
+                username=username, email=email, cpf=cpf, password=password
+            )
+            account.is_active = False
+            account.save()
+
+            subject = "Confirmação de Registro"
+            message = (
+                "Obrigado por se registrar! Por favor, ative sua conta clicando no link abaixo:\n\n"
+                f"http://{request.get_host()}/account/confirm/{account.id}/"
+            )
+            from_email = settings.DEFAULT_FROM_EMAIL
+            to_email = form.cleaned_data.get("email")
+
+            send_mail(subject, message, from_email, [to_email])
+
+            return render(request, "registration/confirmation_sent.html")
+
+        return render(request, "registration/register.html", {"form": form})
 
 
-@require_POST
-def login_process(request):
-    form = LoginForm(request.POST)
-    if form.is_valid():
-        email = form.cleaned_data.get("email")
-        password = form.cleaned_data.get("password")
+class LoginView(View):
+    def get(self, request, *args, **kwargs):
+        form = LoginForm()
+        return render(request, "login/login_form.html", {"form": form})
 
-        if not Account.objects.filter(email=email).exists():
-            messages.error(request, "Conta não cadastrada.")
-            return redirect("registration_form")
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
 
-        account = authenticate(request, email=email, password=password)
+            if not Account.objects.filter(email=email).exists():
+                messages.error(request, "Conta não cadastrada.")
+                return redirect("register")
 
-        if account:
-            if not account.is_active:
-                messages.error(
-                    request, "Por favor, confirme seu e-mail antes de fazer login."
-                )
-                return redirect("login_form")
+            account = authenticate(request, email=email, password=password)
 
-            login(request, account)
-            return redirect("home")
+            if account:
+                if not account.is_active:
+                    messages.error(
+                        request, "Por favor, confirme seu e-mail antes de fazer login."
+                    )
+                    return redirect("login")
 
-    return redirect("login_form")
+                login(request, account)
+                return redirect("home")
 
-
-@require_GET
-def registration_form(request):
-    form = AccountRegistrationForm()
-    return render(request, "registration/register.html", {"form": form})
+        return redirect("login")
 
 
-@require_POST
-def process_registration(request):
-    form = AccountRegistrationForm(request.POST)
+class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
 
-    if form.is_valid():
-        email = form.cleaned_data.get("email")
-        username = form.cleaned_data.get("username")
-        cpf = form.cleaned_data.get("cpf")
-        password = form.cleaned_data.get("password")
+        return redirect("login")
 
-        account = Account.objects.create_user(
-            username=username, email=email, cpf=cpf, password=password
-        )
-        account.is_active = False
+
+class ConfirmView(View):
+    def get(request, *args, **kwargs):
+        account_id = kwargs["account_id"]
+        
+        account = get_object_or_404(Account, id=account_id)
+        account.is_active = True
         account.save()
 
-        subject = "Confirmação de Registro"
-        message = (
-            "Obrigado por se registrar! Por favor, ative sua conta clicando no link abaixo:\n\n"
-            f"http://{request.get_host()}/account/confirm/{account.id}/"
-        )
-        from_email = settings.DEFAULT_FROM_EMAIL
-        to_email = form.cleaned_data.get("email")
-
-        send_mail(subject, message, from_email, [to_email])
-
-        return render(request, "registration/confirmation_sent.html")
-
-    return render(request, "registration/register.html", {"form": form})
+        return redirect("login")
 
 
-def confirm_registration(request, account_id):
-
-    account = get_object_or_404(Account, id=account_id)
-    account.is_active = True
-    account.save()
-
-    return redirect("login_form")
-
-
-@require_GET
-def home_view(request):
-    return render(request, "home.html")
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "home.html")
