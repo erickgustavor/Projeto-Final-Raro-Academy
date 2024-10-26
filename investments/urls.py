7fe1b39edde4bd46857335fb2bcf585f0713a0d0
Drolpg <leandronascimento.drolpg21@gmail.com>
2024-10-26 10:53:03 -0300

    refactor: Utilização de classviews

--- investments/urls.py
+++ investments/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import ProductListView
 
 urlpatterns = [
-    path("products/", views.product_list, name="product_list"),
+    path("products/", ProductListView.as_view(), name="product_list"),
 ]
--- investments/views.py
+++ investments/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 from .models import ProductInvestment
 
 
-def product_list(request):
-    products = ProductInvestment.objects.all()
-    context = {
-        "products": products,
-    }
-    return render(request, "product_list.html", context)
+class ProductListView(View):
+    def get(self, request, *args, **kwargs):
+        products = ProductInvestment.objects.all()
+        context = {"products": products}
+        return render(request, "product_list.html", context)
