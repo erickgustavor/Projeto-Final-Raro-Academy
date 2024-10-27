caae1aa1f396efc897cd701ffb642ac7a59537f5
m3l4o <joaovictor.slv@ufpi.edu.br>
2024-10-27 01:55:40 -0300

    refactor: mudanca nas labels do django admin

--- account/models.py
+++ account/models.py
@@ -19,15 +19,20 @@ class AccountType(Enum):
 
 
 class Account(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, verbose_name="nome de usuário")
     email = models.EmailField(max_length=100, unique=True)
-    cpf = models.CharField(max_length=11, unique=True)
-    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    cpf = models.CharField(max_length=11, unique=True, verbose_name="CPF")
+    balance = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0.0, verbose_name="saldo"
+    )
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
     type = models.CharField(
-        max_length=10, choices=AccountType.choices(), default=AccountType.FREE
+        max_length=10,
+        choices=AccountType.choices(),
+        default=AccountType.FREE,
+        verbose_name="tipo de conta",
     )
 
     USERNAME_FIELD = "email"
