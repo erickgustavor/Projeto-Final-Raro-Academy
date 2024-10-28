db0818e49fec3467d470dbda19f089bca11833a1
m3l4o <joaovictor.slv@ufpi.edu.br>
2024-10-28 12:56:15 -0300

    feat: carregando variaveis de ambiente

--- caps_bank/__init__.py
+++ caps_bank/__init__.py
@@ -1,3 +1,17 @@
+import os
+
 from .celery import app as celery_app
 
-__all__ = ('celery_app',)
+
+def load_env_file(filepath=".env"):
+    if os.path.exists(filepath):
+        with open(filepath) as f:
+            for line in f:
+                if line.strip() and not line.startswith("#"):
+                    key, value = line.strip().split("=", 1)
+                    os.environ[key] = value
+
+
+load_env_file()
+
+__all__ = ("celery_app",)
