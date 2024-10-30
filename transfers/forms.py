43533e9aa7ac02d79615cb6fc6404beed9148d5d
m3l4o <joaovictor.slv@ufpi.edu.br>
2024-10-30 11:05:22 -0300

    fix: correcao ao pegar as contas de contato

--- transfers/forms.py
+++ transfers/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from .models import Account, Transaction 
+from .models import Account, Transaction
+
 
 class TransactionForm(forms.Form):
     to_account = forms.ModelChoiceField(
@@ -8,7 +9,7 @@ class TransactionForm(forms.Form):
         queryset=Account.objects.none(),
         required=True,
         empty_label="Selecione a conta de destino",
-        to_field_name="cpf" 
+        to_field_name="cpf",
     )
 
     amount = forms.DecimalField(
@@ -16,19 +17,23 @@ class TransactionForm(forms.Form):
         max_digits=10,
         decimal_places=2,
         required=True,
-        widget=forms.NumberInput(attrs={'placeholder': 'Insira o valor da transferência'})
+        widget=forms.NumberInput(
+            attrs={"placeholder": "Insira o valor da transferência"}
+        ),
     )
 
     def __init__(self, *args, user=None, **kwargs):
-        super().__init__(*args, **kwargs)
+        super(TransactionForm, self).__init__(*args, **kwargs)
         if user:
-          
-            transacoes = Transaction.objects.filter(from_account=user.account).values_list('to_account', flat=True)
-            self.fields['to_account'].queryset = Account.objects.filter(id__in=transacoes)
+            to_accounts_cpf = Transaction.objects.filter(from_account=user).values_list(
+                "to_account", flat=True
+            )
+            to_accounts = Account.objects.filter(cpf__in=to_accounts_cpf)
+            self.fields["to_account"].queryset = to_accounts
 
     def clean(self):
         cleaned_data = super().clean()
-        to_account = cleaned_data.get('to_account')
+        to_account = cleaned_data.get("to_account")
         if not to_account:
-            raise ValidationError('Conta de destino não encontrada')
+            raise ValidationError("Conta de destino não encontrada")
         return cleaned_data
--- transfers/views.py
+++ transfers/views.py
@@ -1,84 +1,94 @@
-from django.shortcuts import render, redirect
+from decimal import Decimal
+
 from django.conf import settings
-from django.views import View
-from django.utils import timezone
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.mail import send_mail
-from .models import Transaction
+from django.shortcuts import redirect, render
+from django.utils import timezone
+from django.views import View
+
 from account.models import Account
 from transfers.models import Transaction
-from .forms import TransactionForm
 from transfers.services.transaction_token_service import TransactionTokenService
-from decimal import Decimal
-from django.contrib import messages
+
+from .forms import TransactionForm
+from .models import Transaction
 
 
-class TransactionView(View):
-    def get(self, request):
-        from_account = request.user.account  
-        
-        transactions = Transaction.objects.filter(from_account=from_account) | Transaction.objects.filter(to_account=from_account)
-
-        form = TransactionForm()
-        return render(request, 'transaction_form.html', {'form': form, 'transactions': transactions})
-
+class TransactionView(LoginRequiredMixin, View):
     def post(self, request):
-        form = TransactionForm(request.POST)
-        
+        form = TransactionForm(request.POST, user=request.user)
+
         if form.is_valid():
-            from_account = request.user  
-            to_account_cpf = form.cleaned_data['to_account']
-            to_account = Account.objects.get(cpf=to_account_cpf)
-            amount = form.cleaned_data['amount']
+            from_account = request.user
+            to_account = form.cleaned_data["to_account"]
+            amount = form.cleaned_data["amount"]
 
             if from_account.balance >= amount:
 
                 transaction_data = {
-                    'from_account': from_account,
-                    'to_account': to_account.id,
-                    'amount': float(amount)
+                    "from_account": from_account,
+                    "to_account": to_account.id,
+                    "amount": float(amount),
                 }
                 token_service = TransactionTokenService(transaction_data)
                 token_service.generate_token()
 
-                request.session['transaction_data'] = {
-                    'from_account_id': from_account.id,
-                    'to_account_id': to_account.id,
-                    'amount': float(amount)
+                request.session["transaction_data"] = {
+                    "from_account_id": from_account.id,
+                    "to_account_id": to_account.id,
+                    "amount": float(amount),
                 }
-                request.session['transaction_token'] = token_service.token
-                request.session['token_expiration'] = token_service.token_expiration.strftime("%Y-%m-%d %H:%M:%S")
+                request.session["transaction_token"] = token_service.token
+                request.session["token_expiration"] = (
+                    token_service.token_expiration.strftime("%Y-%m-%d %H:%M:%S")
+                )
 
-                return redirect('confirm_transaction')  
+                return redirect("confirm_transaction")
             else:
-                form.add_error(None, 'Saldo insuficiente para realizar a transação.')
+                form.add_error(None, "Saldo insuficiente para realizar a transação.")
 
         for error in form.non_field_errors():
             messages.error(request, error)
             print(error)
 
-        return redirect('home')
-    
+        return redirect("home")
 
-class ConfirmTransactionView(View):
+
+class ConfirmTransactionView(LoginRequiredMixin, View):
     def get(self, request):
 
-        return render(request, 'confirm_transaction.html')
+        return render(request, "confirm_transaction.html")
 
     def post(self, request):
-        token_input = request.POST.get('token')
-        transaction_data = request.session.get('transaction_data')
-        token = request.session['transaction_token']
-        token_expiration = timezone.make_aware(timezone.datetime.strptime(request.session.get('token_expiration'), "%Y-%m-%d %H:%M:%S"))
+        token_input = request.POST.get("token")
+        transaction_data = request.session.get("transaction_data")
+        token = request.session["transaction_token"]
+        token_expiration = timezone.make_aware(
+            timezone.datetime.strptime(
+                request.session.get("token_expiration"), "%Y-%m-%d %H:%M:%S"
+            )
+        )
 
         if timezone.now() > token_expiration:
-            return render(request, 'confirm_transaction.html', {'error': 'O token expirou. Solicite uma nova transação.'})
+            return render(
+                request,
+                "confirm_transaction.html",
+                {"error": "O token expirou. Solicite uma nova transação."},
+            )
 
         if token == token_input:
-            from_account = Account.objects.get(id=transaction_data['from_account_id'])
-            to_account = Account.objects.get(id=transaction_data['to_account_id'])
-            amount = Decimal(transaction_data['amount'])
+            from_account = Account.objects.get(id=transaction_data["from_account_id"])
+            to_account = Account.objects.get(id=transaction_data["to_account_id"])
+            amount = Decimal(transaction_data["amount"])
 
-            transaction = Transaction(from_account=from_account, to_account=to_account, amount=amount, token=token)
+            transaction = Transaction(
+                from_account=from_account,
+                to_account=to_account,
+                amount=amount,
+                token=token,
+            )
             transaction.save()
 
             from_account.balance -= amount
@@ -87,22 +97,26 @@ class ConfirmTransactionView(View):
             to_account.save()
 
             send_mail(
-                'Transação Confirmada',
-                f'Sua transação de R$ {amount:.2f} para {to_account.username} (CPF {to_account.cpf}) foi realizada com sucesso.',
+                "Transação Confirmada",
+                f"Sua transação de R$ {amount:.2f} para {to_account.username} (CPF {to_account.cpf}) foi realizada com sucesso.",
                 settings.DEFAULT_FROM_EMAIL,
                 [from_account.email],
                 fail_silently=False,
             )
 
             send_mail(
-                'Você recebeu uma transação',
-                f'Você recebeu R$ {amount:.2f} de {from_account.username} (CPF {from_account.cpf}).',
+                "Você recebeu uma transação",
+                f"Você recebeu R$ {amount:.2f} de {from_account.username} (CPF {from_account.cpf}).",
                 settings.DEFAULT_FROM_EMAIL,
                 [to_account.email],
                 fail_silently=False,
             )
 
-            request.session['success_message'] = "Transação confirmada com sucesso!"
-            return redirect('home')
+            request.session["success_message"] = "Transação confirmada com sucesso!"
+            return redirect("home")
         else:
-            return render(request, 'confirm_transaction.html', {'error': 'Token inválido. Verifique o token e tente novamente.'})
+            return render(
+                request,
+                "confirm_transaction.html",
+                {"error": "Token inválido. Verifique o token e tente novamente."},
+            )
