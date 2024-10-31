203fd7e724595e6ff6b944e12bf9261e962da56b
erickgustavor <erick.gustavo.r@gmail.com>
2024-10-31 14:56:31 -0300

    Initial commit

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.urls import path
+from django.template.response import TemplateResponse
 from .models import Account
-
+from django.db.models import Sum
+from investments.models import  Investment
 
 class AccountAdmin(admin.ModelAdmin):
     list_display = ("username", "type", "balance", "display_groups")
@@ -16,5 +19,33 @@ class AccountAdmin(admin.ModelAdmin):
 
     display_groups.short_description = "Turmas"
 
-
 admin.site.register(Account, AccountAdmin)
+
+class CustomAdminSite(admin.AdminSite):
+    def get_urls(self):
+
+        default_urls = super().get_urls()  
+
+        custom_urls = [
+            path("", self.admin_view(self.dashboard_view), name="dashboard"),
+        ]
+        return custom_urls + default_urls
+
+    def dashboard_view(self, request):
+
+        total_active_users = Account.objects.filter(is_active=True).count()
+        premium_users = Account.objects.filter(type='premium').count()
+        total_invested = Investment.objects.aggregate(total=Sum('applied_value'))['total'] or 0
+
+        context = dict(
+            self.each_context(request),
+            total_active_users=total_active_users,
+            premium_users=premium_users,
+            total_invested=total_invested,
+        )
+        return TemplateResponse(request, "admin/custom_dashboard.html", context)
+
+
+custom_admin_site = CustomAdminSite(name='custom_admin')
+custom_admin_site.register(Account, AccountAdmin)
+admin.site = custom_admin_site
